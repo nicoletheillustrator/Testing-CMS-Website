3de9b9b52801fbac7ad60519efa649da563009d2
nicoletheillustrator <nicole-park@naver.com>
2022-11-23 17:52:05 +0900

    Added basic mapping to the file.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./main"
-
-
+import charData from './data/learning.json'
+import Card from './card'
 function App() {
+  const charArr = charData.characters
+
+
+  const card = charArr.map((item, index) => {
+   return (
+   <Card 
+      key = {index}
+      data = {item}
+    />
+    )
+  })
+
   return (
     <div className="App">
       <h1>Button for adding blogs.</h1>
       <p>A start to learning headless CMS and using JSON for future clients. The main purpose is to be able to push strings of the same nature to a JSON file that will then be mapped and used to create component cards to show off blogs in a list/grid</p>
-      <button >Push Button</button>
+      {card}
     </div>
   );
 }
